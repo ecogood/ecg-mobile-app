03d8a70740360f632a58dbfdcfd12dbf191197ee
Nikolay Georgiev <nikolay@georgiev.cc>
2014-10-21 20:23:29 +0200

    sample matrix with indicators

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -12,7 +12,9 @@
       { id: 0, name: 'A1: Ethisches Beschaffungswesen' },
       { id: 1, name: 'B1: Ethisches Finanzmanagement' },
       { id: 2, name: 'C1: Arbeitsplatzqualität und Gleichstellung' },
-      { id: 3, name: 'C2: Gerechte Verteilung der Erwerbsarbeit' }
+      { id: 3, name: 'C2: Gerechte Verteilung der Erwerbsarbeit' },
+      { id: 4, name: 'D5: Erhöhung der sozialen und ökologischen Branchenstandards' },
+      { id: 5, name: 'E1: Sinn und Gesellschaftliche Wirkung der Produkte / Dienstleistungen' }
     ];
 
     return {
